a46e3710ee377e946c29fc332a3837451f5d50cf
Livelycobra <connorgenyk@gmail.com>
2018-02-26 20:15:26 -0600

    Initial commit

---
+++ src/MainGUI.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+public class MainGUI {
+    private JTabbedPane tabbedPane2;
+    private JTabbedPane tabbedPane1;
+    private JTabbedPane tabbedPane4;
+    private JTabbedPane tabbedPane5;
+    private JTextArea DisplayAcN;
+    private JTextArea DisplayName;
+    private JTextArea DisplayBalance;
+    private JPasswordField passwordField1;
+    private JButton searchButton;{
+        JOptionPane.showMessageDialog(null, "Test");
+    }
+    private JTextField textField1;{
+    }
+    private JButton depositToChqAcctButton;
+    private JButton depositToSavingsAcctButton;
+    private JTabbedPane tabbedPane3;
+}

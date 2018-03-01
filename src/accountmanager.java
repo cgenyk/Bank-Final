6ea463704cd0f31e7788584dec0607672ef97e1a
Connor Genyk <genyk-c@webmail.uwinnipeg.ca>
2018-03-01 10:05:29 -0600

    Major Commit - Redid account manager class file.

--- src/accountmanager.java
+++ src/accountmanager.java
@@ -1,69 +1,49 @@
-import java.util.Arrays;
-
-public class accountmanager {
-
-    public static String getname() {
-        String HRX = "Mr";
-        String firstname = "Noah";
-      String lastname  = "Freeman";
-      String MI = "A";
-      String Suffix = "Jr";
-      String LegalName = ("Account Holder: " + "\n Honorific : " + HRX + "\n Last Name: " + lastname + "\n First Name: " + firstname +"\n Suffix: "+ Suffix + "\n MI: " + MI);
-      return LegalName;
-    }
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 
-
-    public static String getadr() {
-        String NUM = "198";
-        String STR = "Main ST";
-        String PROV  = "MB";
-        String POST = "R3C 1A3";
-        String COUNT = "CAN";
-        String Address = ("Address: " + "\n Building Number: " + NUM + "\n Street: " + STR + "\n Provence: " + PROV +"\n Postal Code: "+ POST + "\n Country: " + COUNT);
-        return Address;
-    }
-
-    public static String getcontact() {
-        String EM1 = "n.freman@anycorp.com";
-        String EM2 = "noahfreman@gmail.com";
-        String PH1 = "(1)204-154-7589";
-        String PH2 = "(1)204-324-3459";
-        String PC = "EM1";
-        String contact = ("Contact Method : " + "\n Email 1: " + EM1 + "\n Email 2: " + EM2 + "\n Phone 1: " + PH1 + "\n Phone 2: " + PH2 + "\n Pref Contact: " + PC);
-        return contact;
-
-    }
+// static method, usually the class name is differnet
+public class AccountData {
+    class Account {
+        public int getId(){
 
 
+            return id;
+        }
+        public String getname(){
 
-        public static String nad(){
-            String Warnings = "!!! - (1) 27/02/17 - Lost Card Found.";
-            String Advisories = "*** - (1) 27/02/17 - [Deal Ending] Student Advantage Plus is ending this month.";
-            String Notes  = "(1) 27/02/17 - Left gloves in lobby, placed behind desk for pick up.";
-            String note = ("Notes and Information : " + "\n Warnings: " + Warnings + "\n Advisories: " + Advisories + "\n Notes: " + Notes);
-            return note;
-
-
+            return name;
         }
 
-
-
-    public static boolean ACN(char[] input) {
-        boolean found = true;
-        char[] ACN = {'1', '2', '3', '4',};
-
-        if (input.length != ACN.length) {
-            found = false;
-        } else {
-            found = Arrays.equals(input, ACN);
+        public String getaddress(){
+           return address;
         }
-        return found;
+        public String getcontact(){
+            return contact;
+
+        }
+        public String getaccounts() {
+            return account;
+
+        }
+        public Double getballancechq() {
+            return chqbal;
+
+        }
+        public Double getballancesav() {
+            return savbal;
+
+        }
     }
+    List<Account> accounts = new ArrayList<Account>();
+
+    public static String loadDataFromFile(File file) {
+
+        Scanner scan = new Scanner("src/accountdata.csv");
 
 
+    }
 }
-
-// Important Note: Code for "ACN" was based off of https://docs.oracle.com/javase/tutorial/uiswing/components/passwordfield.html
-
 
